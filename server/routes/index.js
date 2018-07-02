261e949b40141ac3de594d57ee3e0bb4a6f954e7
Gbaja <gbajaf@yahoo.co.uk>
2018-07-02 13:06:29 +0100

    Backend api call

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
 const router = express.Router();
+const airtable = require("../utils/airtable");
 
-router.get("/", (req, res) => {
-    res.send("Hello world");
+router.get("/api/all_organisations", (req, res) => {
+    airtable.getOrganisations().then(organisations =>{
+      res.send(organisations)
+    })
 });
+
+router.get("/api/one_organisation", (req, res) => {
+    airtable.getOneOrganisation("Futureversity").then(organisation =>{
+        res.send(organisation)
+    })
+})
 
 module.exports = router;
---
+++ server/utils/airtable.js
@@ -0,0 +1,55 @@
+const Airtable = require("airtable");
+require("env2")("config.env");
+
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+
+const base = Airtable.base(process.env.AIRTABLE_BASE);
+const ORGANISATION_BASE = "Organisations";
+
+const OrganisationFields = [
+    "Name of Organisation",
+    "Type of Organisation",
+    "Registered number",
+    "Website",
+    "Logo",
+    "Organisation email",
+    "Organisation contact number",
+    "About",
+    "Areas we work in",
+    "Services Provided to young people",
+    "How we work with young people",
+    "Age group we work with"
+]
+
+const getOrganisations = () => 
+    base(ORGANISATION_BASE)
+        .select({
+            filterByFormula: `{Status} = "Verified"`,
+            fields: ["Name of Organisation",
+                    "Type of Organisation",
+                     "Services Provided to young people", ]
+        })
+        .all()
+        .then(records => {
+        return records.map(record => record.fields);
+        })
+        .catch(err=> {
+            console.log(err)
+        });
+
+const getOneOrganisation = org_name => 
+    base(ORGANISATION_BASE)
+        .select({
+            filterByFormula: `{Name of Organisation} = \"${org_name}\"`,
+            fields: OrganisationFields
+        })
+        .all()
+        .then(([record]) => record.fields)
+
+module.exports = {
+  getOrganisations,
+  getOneOrganisation
+};
