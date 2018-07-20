01074f7f672bdb6d9118517d4c5f6e752a7f56f4
Gbaja <gbajaf@yahoo.co.uk>
2018-07-20 14:56:09 +0100

    Made a joinform functional component

--- src/Join/JoinForm.js
+++ src/Join/JoinForm.js
@@ -1,121 +1,78 @@
-import React from "react";
-import { Form, Field } from "formik";
+import React, {Fragment} from "react";
+
 
 import "./Join.css"
 import Header from "../StaticPages/Header"
 
-const JoinForm = ({ errors, touched, status }) => {
-  const { 
-    orgName, 
-    orgType, 
-    registeredNumber, 
-    website, 
-    orgEmail, 
-    orgNumber, 
-    about, 
-    areas, 
-    ageGroup,
-    mentoring,
-    housing,
-    events,
-    employability,
-    intern,
-    volunteering,
-    mental,
-    legal,
-    other,
-    referrals,
-    schools,
-    direct, 
-    completedBy
-    
-  } = errors;
-    return <div>
-        <Form>
-          <Header />
-          <h3>Join BridgeMap List</h3>
-          <label>Name of Organisation</label>
-          {touched.orgName && orgName && <p>{orgName}</p>}
-          <p />
-          <Field type="text" name="orgName" placeholder="orgName" />
-          <label>Type of Organisation</label>
-          <p />
-          {touched.orgType && orgType && <p>{orgType}</p>}
-          <Field component="select" name="orgType">
-            <option value="" />
-            <option value="Charity">Charity</option>
-            <option value="Social enterprise/Not for Profit">
-              Social enterprise/Not for Profit
-            </option>
-            <option value="Community Interest Company">
-              Community Interest Company
-            </option>
-            <option value="Other i.e Partnership, Limited Company, Sole trader ">
-              Other i.e Partnership, Limited Company, Sole trader{" "}
-            </option>
-          </Field>
-          <label>Registered number</label>
-          {touched.registeredNumber && registeredNumber && <p>
-                {registeredNumber}
-              </p>}
-          <p />
-          <Field type="test" name="registeredNumber" placeholder="registeredNumber" />
-          <label>Website</label>
-          {touched.website && website && <p>{website}</p>}
-          <p />
-          <Field type="text" name="website" placeholder="website" />
-          <label>Organisation email</label>
-          {touched.orgEmail && orgEmail && <p>{orgEmail}</p>}
-          <p />
-          <Field type="email" name="orgEmail" placeholder="orgEmail" />
-          <label>Organisation contact number</label>
-          {touched.orgNumber && orgNumber && <p>{orgNumber}</p>}
-          <p />
-          <Field type="tel" name="orgNumber" placeholder="orgNumber" />
-          <label>About</label>
-          {touched.about && about && <p>{about}</p>}
-          <p />
-          <Field type="textArea" name="about" placeholder="about" />
-          <label>Areas we work in</label>
-          <p />
-          {touched.areas && areas && <p>{areas}</p>}
-          <Field type="textArea" name="areas" placeholder="areas" />
-          <p>Services provided to young people</p>
-          <p />
-          <label>Mentoring</label>
-          <Field type="checkbox" name="mentoringService" />
-          <label>Housing</label>
-          <Field type="checkbox" name="housingService" />
-          <label>Short courses, events and activities</label>
-          <Field type="checkbox" name="eventsService" />
-          <label>Employability advice and support</label>
-          <Field type="checkbox" name="employabilityService" />
-          <label>Paid Apprenticeship/Internship</label>
-          <Field type="checkbox" name="internService" />
-          <label>Volunteering</label>
-          <Field type="checkbox" name="volunteeringService" />
-          <label>Mental health support</label>
-          <Field type="checkbox" name="mentalService" />
-          <label>Legal advice</label>
-          <Field type="checkbox" name="legalService" />
-          <p>How we work with young people</p>
-          <p />
-          <label>Referalls</label>
-        <Field type="checkbox" name="throughReferrals" />
-          <label>Through Partnership with schools</label>
-        <Field type="checkbox" name="throughSchools" />
-          <label>Young people can come to us directly</label>
-        <Field type="checkbox" name="throughDirect" />
-          <label>Age group we work with</label>
-          {touched.ageGroup && ageGroup && <p>{ageGroup}</p>}
-          <p />
-          <Field type="text" name="ageGroup" placeholder="ageGroup" />
-          <label>Form completed by</label>
-          {touched.completedBy && completedBy && <p>{completedBy}</p>}
-          <Field type="text" name="completedBy" />
-          <button>Submit</button>
-        </Form>
-      </div>;
+const JoinForm = (props) => {
+  return (
+    <div>
+    <h2>Join BridgeMap List</h2>
+    <form onSubmit={props.handleSumbit}>
+      <label>Organisation name</label>
+      <input name="orgName"
+        type="text"
+        value={props.form.orgName}
+        onChange={props.handleInputChange}
+      />
+      <label>Organisation Type</label>
+      <select
+        name="orgType"
+        value={props.form.orgType}
+        onChange={props.handleInputChange}>
+        <option value=""></option>
+        <option value="Charity">Charity</option>
+        <option value="Social enterprise/Not for Profit">Social enterprise/Not for Profit</option>
+        <option value="Community Interest Company">Community Interest Company</option>
+        <option value="Other i.e partnership, sole trader">Other i.e partnership, sole trader</option>
+      </select>
+      <label>Logo</label>
+      <input
+        name="logo"
+        type="file"
+
+        onChange={props.handleFileUpload}
+      />
+      <label>Services we offer to young people</label>
+      {props.services.map(({ Name }) => {
+        return <Fragment key={Name}>
+          <label>{Name}</label>
+          <input
+            type="checkbox"
+            value={Name}
+            checked={props.form.services.includes(Name)}
+            onChange={props.handleServiceChange}
+          />
+        </Fragment>
+      })}
+      <label>How we work with young people</label>
+      {props.how.map(({ Name }) => {
+        return <Fragment key={Name}>
+          <label>{Name}</label>
+          <input
+            type="checkbox"
+            value={Name}
+            checked={props.form.how.includes(Name)}
+            onChange={props.handleHowChange}
+          />
+        </Fragment>
+      })}
+      <label>Areas we work within</label>
+      {props.where.map(({ Name }) => {
+        return <Fragment key={Name}>
+          <label>{Name}</label>
+          <input
+            type="checkbox"
+            value={Name}
+            checked={props.form.where.includes(Name)}
+            onChange={props.handleWhereChange}
+          />
+        </Fragment>
+      })}
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+  )
 };
 
 export default JoinForm;
--- src/Join/JoinFormContainer.js
+++ src/Join/JoinFormContainer.js
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from "react"
 
 import Header from "../StaticPages/Header";
 import {fetchServices, fetchHows, fetchWheres} from "../requests/airtable"
+import JoinForm from "./JoinForm"
 import "./Join.css";
 
 const byNotEqualTo = value => service => service !== value
@@ -16,7 +17,9 @@ class JoinFormContainer extends Component {
         orgName: "",
         orgType: "",
         logo: "",
-        services: []
+        services: [],
+        how: [],
+        where: []
       }
   }
 
@@ -59,194 +62,33 @@ class JoinFormContainer extends Component {
       console.log(value, checked)
   }
 
+  handleHowChange = ({target: {checked, value}}) => {
+    const {form} = this.state;
+    const {how} = form
+    if(checked){
+      this.setState({form: {...form, how: [...how, value]}})
+    } else {
+      this.setState({form: {...form, how: how.filter(byNotEqualTo(value))}})
+    }
+  }
+
+  handleWhereChange = ({target: {checked, value}}) => {
+    const { form } = this.state;
+    const { where } = form
+    if (checked) {
+      this.setState({ form: { ...form, where: [...where, value] } })
+    } else {
+      this.setState({ form: { ...form, where: where.filter(byNotEqualTo(value)) } })
+    }
+  }
   render(){
     if(this.state.loading){
       return <div>loading form...</div>
     }
     return <div>
-      <Header/>
-      <h2>Join BridgeMap List</h2>
-        <form onSubmit={this.handleSumbit}>
-          <label>Organisation name</label>
-          <input name="orgName" 
-            type="text" 
-            value={this.state.form.orgName} 
-            onChange={this.handleInputChange} 
-          />
-          <label>Organisation Type</label>
-          <select 
-            name="orgType"
-            value={this.state.form.orgType} 
-            onChange={this.handleInputChange}>
-              <option value=""></option>
-              <option value="Charity">Charity</option>
-              <option value="Social enterprise/Not for Profit">Social enterprise/Not for Profit</option>
-              <option value="Community Interest Company">Community Interest Company</option>
-              <option value="Other i.e partnership, sole trader">Other i.e partnership, sole trader</option>
-          </select>
-          <label>Logo</label>
-          <input 
-            name="logo"
-            type="file"
-            
-            onChange={this.handleFileUpload}
-          />
-        <label>Services we offer to young people</label>
-        {this.state.services.map(({Name}) => {
-          return <Fragment key={Name}>
-            <label>{Name}</label>
-            <input
-              type="checkbox"
-              value={Name}
-              checked={this.state.form.services.includes(Name)}
-              onChange={this.handleServiceChange}
-            />
-          </Fragment>
-        })}
-           {/* <label>Services we offer to young people</label>
-          {this.state.services.map(service=>{
-            return <Fragment><label>{service.Name}</label>
-              <input
-                type="checkbox"
-                // value={this.state.orgName}
-                // onChange={this.handleInputChange}
-              />
-              </Fragment>
-        })}
-        <label>How we work with young people</label>
-        {this.state.how.map(how => {
-          return <Fragment><label>{how.Name}</label>
-            <input
-              type="checkbox"
-            // value={this.state.orgName}
-            // onChange={this.handleInputChange}
-            />
-          </Fragment>
-        })}
-        <label>Areas we work within</label>
-        {this.state.where.map(where => {
-          return <Fragment><label>{where.Name}</label>
-            <input
-              type="checkbox"
-            // value={this.state.orgName}
-            // onChange={this.handleInputChange}
-            />
-          </Fragment>
-        })}  */}
-          <button type="submit">Submit</button>
-        </form>
+      <JoinForm form={this.state.form} handleInputChange={this.handleInputChange} handleServiceChange={this.handleServiceChange} handleHowChange={this.handleHowChange} handleWhereChange={this.handleWhereChange} handleSumbit={this.handleSumbit} handleFileUpload={this.handleFileUpload} services={this.state.services} where={this.state.where} how={this.state.how}/>
       </div>;
   }
 }
 
 export default JoinFormContainer
-// import { withFormik } from "formik";
-// import * as Yup from "yup";
-// import axios from "axios"
-
-// import JoinForm from "./JoinForm";
-// import {addOrganisation} from "../requests/airtable"
-
-// const JoinFormForkik = withFormik({
-//   mapPropsToValues({ 
-//       orgName,
-//       orgType,
-//       registeredNumber,
-//       website,
-//       orgEmail,
-//       orgNumber,
-//       about,
-//       areas,
-//       ageGroup,
-//       completedBy,
-//     mentoringService,
-// housingService,
-// eventsService,
-// employabilityService,
-// internService,
-// volunteeringService,
-// mentalService,
-// legalService,
-// otherService,
-//       throughReferrals,
-//       throughSchools,
-//       throughDirect
-//     }) {
-//     return {
-//       orgName: orgName || "",
-//       orgType: orgType || "",
-//       registeredNumber: registeredNumber ||"",
-//       website: website ||"",
-//       orgEmail: orgEmail ||"",
-//       orgNumber: orgNumber ||"",
-//       about: about ||"",
-//       areas: areas ||"",
-//       ageGroup: ageGroup || "",
-//       completedBy: completedBy || "",
-//       mentoringService: mentoringService || "",
-// housingService: housingService || "",
-// eventsService:eventsService || "",
-// employabilityService: employabilityService || "",
-// internService: internService || "",
-// volunteeringService: volunteeringService || "",
-// mentalService: mentalService || "",
-// legalService: legalService || "",
-// otherService: otherService||"",
-//       throughReferrals:throughReferrals ||"",
-//       throughSchools: throughSchools||"",
-//       throughDirect:throughDirect || ""    };
-//   },
-//   validationSchema: Yup.object().shape({
-//     // name: Yup.string().required("Please enter a name!"),
-//     // userName: Yup.string().required("Please enter a userName!"),
-//     // email: Yup.string()
-//     //   .email("Email not valid")
-//     //   .required("Email is required"),
-//     // password: Yup.string().required("Please enter a password!"),
-//     // isExpert: Yup.string().required("Please select a value")
-//   }),
-//   handleSubmit(values, { props, setStatus }) {
-//     console.log(values);
-//     let services = [];
-//     let how = [];
-//     if(values.mentoringService){
-//       services.push("Mentoring")
-//     }
-//     if(values.housingService){
-//       services.push("Housing")
-//     }
-//     if(values.eventsService){
-//       services.push("Short courses, events and activities")
-//     }
-//     if(values.employabilityService){
-//       services.push("Employability advice and support");
-//     }
-//       if (values.internService){
-//         services.push("Paid Apprenticeship/Internship");
-//       }
-//       if(values.volunteeringService){
-//         services.push("Volunteering")
-//       }
-//       if(values.mentalService){
-//         services.push("Mental health support");
-//       }
-//       if(values.legalService){
-//         services.push("Legal advice")
-//       }
-//       if(values.throughReferrals){
-//         how.push("Referrals")
-//       }
-//       if(values.throughSchools){
-//         how.push("Through partnership with schools");
-//       }
-//       if(values.throughDirect){
-//         how.push("Young people can come to us directly");
-//       }
-//     values.services = services;
-//     values.how = how;
-//     return addOrganisation(values)
-   
-//   }
-// })(JoinForm);
-
-// export default JoinFormForkik;
