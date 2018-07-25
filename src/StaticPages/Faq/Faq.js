d5afeefe37fd31df626523d1899bc7282f3716ae
Gbaja <gbajaf@yahoo.co.uk>
2018-07-25 15:38:21 +0100

    faq desktop view css

--- src/StaticPages/Faq/Faq.js
+++ src/StaticPages/Faq/Faq.js
@@ -12,7 +12,8 @@ const Faq = props => {
         <h3 className="faqs__heading">Frequently Asked Questions</h3>
         <p>
           Please see below for some of the questions we are asked often, if you
-          do not find your question, feel free to email us on gbajaf@yahoo.co.uk
+          do not find your question, feel free to email us on{" "}
+          <a href="mailto:gbajaf@yahoo.co.uk">gbajaf@yahoo.co.uk</a>
         </p>
         <ul className="faqs__ul">
           {props.faqs.map((faq, i) => {
