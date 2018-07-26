396d6e276ab40bf705b966f6d824c9cc5d8bf82f
Gbaja <gbajaf@yahoo.co.uk>
2018-07-26 15:44:26 +0100

    minor css

--- src/Shared/Header/SideDrawer/SideDrawer.js
+++ src/Shared/Header/SideDrawer/SideDrawer.js
@@ -13,18 +13,27 @@ const sideDrawer = props => {
       <button onClick={props.closeSideDrawer} className="side-drawer__button">
         X
       </button>
-      <ul>
-        <li>
-          <Link to="/">Directory</Link>
+      <ul className="side-drawer__ul">
+        <li className="side-drawer__list">BridgeMap</li>
+        <li className="side-drawer__list">
+          <Link to="/" className="side-drawer__link">
+            Directory
+          </Link>
         </li>
-        <li>
-          <Link to="/about">About</Link>
+        <li className="side-drawer__list">
+          <Link to="/about" className="side-drawer__link">
+            About
+          </Link>
         </li>
-        <li>
-          <Link to="/faq">FAQ</Link>
+        <li className="side-drawer__list">
+          <Link to="/faq" className="side-drawer__link">
+            FAQ
+          </Link>
         </li>
-        <li>
-          <Link to="/join">Join</Link>
+        <li className="side-drawer__list" className="side-drawer__list">
+          <Link to="/join" className="side-drawer__link">
+            Join
+          </Link>
         </li>
       </ul>
     </nav>
