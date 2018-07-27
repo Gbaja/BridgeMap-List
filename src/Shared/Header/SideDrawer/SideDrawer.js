2d36b218db3a0c6dc6be431ef3e2e0ab1f5b4833
Gbaja <gbajaf@yahoo.co.uk>
2018-07-27 13:12:51 +0100

    side drawer changes

--- src/Shared/Header/SideDrawer/SideDrawer.js
+++ src/Shared/Header/SideDrawer/SideDrawer.js
@@ -1,5 +1,4 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
 import SideDrawerLink from "./SideDrawerLink";
 import "./SideDrawer.css";
