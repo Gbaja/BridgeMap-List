ccf17017c57e5da62ea50d119dc33ba440464902
Gbaja <gbajaf@yahoo.co.uk>
2018-07-30 16:54:30 +0100

    back end for find organosation

--- server/utils/airtable.js
+++ server/utils/airtable.js
@@ -228,28 +228,58 @@ const addOrganisation = async data => {
       console.log("AIRTABLE ADD ORGANISATION ERROR: ", err);
     });
 };
-const findOrganisations = data =>
-  base(HOW_BASE)
+const findOrganisations = async data => {
+  const howData = await base(HOW_BASE)
     .select({
       filterByFormula: `{Name} = \"${data.how}\"`,
       fields: ["Name", "Organisations"]
     })
-    .all()
-    .then(([record]) => {
-      const info = record.fields["Organisations"].map(id => {
-        return base(ORGANISATION_BASE)
-          .find(id)
-          .then(record => {
-            console.log(record.fields);
-            return record.fields;
-          });
-      });
-      Promise.all(info).then(results => {
-        console.log(results);
-      });
-      return record.fields;
-    });
+    .all();
+  const allHowData = howData.map(record => record.fields.Organisations);
 
+  const serviceData = await base(SERVICES_BASE)
+    .select({
+      filterByFormula: `{Name} = \"${data.service}\"`,
+      fields: ["Name", "Organisations"]
+    })
+    .all();
+  const allServiceData = serviceData.map(record => record.fields.Organisations);
+
+  const whereData = await base(WHERE_BASE)
+    .select({
+      filterByFormula: `{Name} = \"${data.where}\"`,
+      fields: ["Name", "Organisations"]
+    })
+    .all();
+  const allWhereData = whereData.map(record => record.fields.Organisations);
+
+  const arrayHow = allHowData[0] || [];
+  const arrayService = allServiceData[0] || [];
+  const arrayWhere = allWhereData[0] || [];
+  const organisationsArrayResults = [
+    ...new Set([...arrayHow, ...arrayService, ...arrayWhere])
+  ];
+  console.log("ORGANISATION ARRAY RESULT: ", organisationsArrayResults);
+  const organisationsResults = organisationsArrayResults.map(organisation => {
+    console.log(organisation);
+    return base(ORGANISATION_BASE)
+      .find(organisation)
+      .then(record => {
+        const neededData = {
+          "Name of Organisation": record.fields["Name of Organisation"],
+          "Type of Organisation": record.fields["Type of Organisation"],
+          "Services Provided to young people":
+            record.fields["Services Provided to young people"],
+          logo: record.fields["logo"]
+        };
+        return neededData;
+      });
+  });
+
+  const searchedOrganisations = await Promise.all(organisationsResults);
+  console.log(searchedOrganisations.length);
+  return searchedOrganisations;
+};
 module.exports = {
   getAllServices,
   getAllHow,
