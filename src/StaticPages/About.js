81ae6742c85f943c24ddc9eb04c71c0f39789c90
Gbaja <gbajaf@yahoo.co.uk>
2018-08-03 15:55:05 +0100

    Minor change to about - still needs work

--- src/StaticPages/About.js
+++ src/StaticPages/About.js
@@ -7,18 +7,18 @@ const About = () => (
   <div className="about__container">
     <Header />
     <p className="about__text">
-      BridgeMap List is an online directory for young people (10 - 25 years old)
-      to use when searching for organisations that can support/advise them on
-      different aspects of their life.
+      BridgeMap List is an online directory of organisations that works with
+      young people. This can be the company's main purpose or a subset of its
+      purpose, as long as you do some work with young people, you can join!
     </p>
     <p className="about__text">
       If you are an organisation and you provide a service for young people, you
       can make sure they know about it by joining this platform!
     </p>
     <p className="about__text">
-      If you are a young person seeking support, advise or information on
-      anything, you can look through our dirctory for an organisation that can
-      support you!
+      If you are looking for an organisation that provides a service, support or
+      advice to young people, you can look through our dirctory for an
+      organisation that can support you!
     </p>
   </div>
 );
