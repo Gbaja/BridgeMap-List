2ce9f85c8c6171f7f77705f2e4fae59b9497cacc
Gbaja <gbajaf@yahoo.co.uk>
2018-08-07 15:09:10 +0100

    Express error handling, not sure if its working

--- server/utils/fileUploadMiddleware.js
+++ server/utils/fileUploadMiddleware.js
@@ -1,23 +1,27 @@
 const cloudinary = require("cloudinary");
 
-const fileUploadMiddleware = (req, res) => {
-  console.log("BBODY: ", req.body);
-  cloudinary.uploader
-    .upload_stream((result, error) => {
-      if (error) {
-        console.log("ERROR:", error);
-      } else {
-        console.log("RESULT: ", result);
-        if (result.error) {
-          return res.status(422).send({
-            type: "upload-error",
-            message: "Logo must be 10MB or less in size."
-          });
+const fileUploadMiddleware = (req, res, next) => {
+  try {
+    console.log("BBODY: ", req.body);
+    cloudinary.uploader
+      .upload_stream((result, error) => {
+        if (error) {
+          console.log("ERROR:", error);
+        } else {
+          console.log("RESULT: ", result);
+          if (result.error) {
+            return res.status(422).send({
+              type: "upload-error",
+              message: "Logo must be 10MB or less in size."
+            });
+          }
+          res.send({ imageUrl: result.secure_url });
         }
-        res.send({ imageUrl: result.secure_url });
-      }
-    })
-    .end(req.file.buffer);
+      })
+      .end(req.file.buffer);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = fileUploadMiddleware;
