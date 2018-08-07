abf0200b3b9c4cbc92a3c431d0be05c7734ce8ec
Gbaja <gbajaf@yahoo.co.uk>
2018-08-07 14:35:10 +0100

    Added try catch to airtable backend code

--- server/utils/airtable/allHow.js
+++ server/utils/airtable/allHow.js
@@ -1,16 +1,20 @@
 const { airtable } = require("./airtable");
 
 const getAllHow = async () => {
-  const allHowRecords = await airtable
-    .base(airtable.HOW_BASE)
-    .select({
-      fields: ["Name"]
-    })
-    .all();
-  const howFields = allHowRecords.map(record => {
-    return { id: record.id, fields: record.fields };
-  });
-  return howFields;
+  try {
+    const allHowRecords = await airtable
+      .base(airtable.HOW_BASE)
+      .select({
+        fields: ["Name"]
+      })
+      .all();
+    const howFields = allHowRecords.map(record => {
+      return { id: record.id, fields: record.fields };
+    });
+    return howFields;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = getAllHow;
--- server/utils/airtable/oneOrganisation.js
+++ server/utils/airtable/oneOrganisation.js
@@ -1,46 +1,50 @@
 const { airtable, OrganisationFields } = require("./airtable");
 
 const getOneOrganisation = async org_name => {
-  const record = await airtable
-    .base(airtable.ORGANISATION_BASE)
-    .select({
-      filterByFormula: `{Name of Organisation} = \"${org_name}\"`,
-      fields: OrganisationFields
-    })
-    .all();
+  try {
+    const record = await airtable
+      .base(airtable.ORGANISATION_BASE)
+      .select({
+        filterByFormula: `{Name of Organisation} = \"${org_name}\"`,
+        fields: OrganisationFields
+      })
+      .all();
 
-  const data = record.map(record => record.fields);
-  const where = data[0]["Where we are based"].map(id => {
-    return airtable
-      .base(airtable.WHERE_BASE)
-      .find(id)
-      .then(record => {
-        return record.fields.Name;
-      });
-  });
-  const how = data[0]["How we work with young people"].map(id => {
-    return airtable
-      .base(airtable.HOW_BASE)
-      .find(id)
-      .then(record => {
-        return record.fields.Name;
-      });
-  });
-  const services = data[0]["Services Provided to young people"].map(id => {
-    return airtable
-      .base(airtable.SERVICES_BASE)
-      .find(id)
-      .then(record => {
-        return record.fields.Name;
-      });
-  });
-  const howArray = await Promise.all(how);
-  const servicesArray = await Promise.all(services);
-  const whereArray = await Promise.all(where);
-  data[0]["Services Provided to young people"] = servicesArray;
-  data[0]["How we work with young people"] = howArray;
-  data[0]["Where we are based"] = whereArray;
-  return data;
+    const data = record.map(record => record.fields);
+    const where = data[0]["Where we are based"].map(id => {
+      return airtable
+        .base(airtable.WHERE_BASE)
+        .find(id)
+        .then(record => {
+          return record.fields.Name;
+        });
+    });
+    const how = data[0]["How we work with young people"].map(id => {
+      return airtable
+        .base(airtable.HOW_BASE)
+        .find(id)
+        .then(record => {
+          return record.fields.Name;
+        });
+    });
+    const services = data[0]["Services Provided to young people"].map(id => {
+      return airtable
+        .base(airtable.SERVICES_BASE)
+        .find(id)
+        .then(record => {
+          return record.fields.Name;
+        });
+    });
+    const howArray = await Promise.all(how);
+    const servicesArray = await Promise.all(services);
+    const whereArray = await Promise.all(where);
+    data[0]["Services Provided to young people"] = servicesArray;
+    data[0]["How we work with young people"] = howArray;
+    data[0]["Where we are based"] = whereArray;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = getOneOrganisation;
